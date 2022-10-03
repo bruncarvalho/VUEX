079a4fc0890673e37e0bb0d830f70d14d944952a
Bruna Carvalho <brunacarvalhocardoso@gmail.com>
2022-10-03 14:28:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <AppProducts/>
+  <br><br>
+  <br><br>
+
+  <pre>
+    {{ $store.state.cart}}
+  </pre>
+
+  <br><br>
+  <br><br>
+  <button @click="updateUser">
+    Atualizar perfil
+  </button>
+
+  
+</template>
+
+<script>
+import AppProducts from './components/Products/AppProducts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppProducts
+},
+  data(){
+    return {
+
+    }
+  },
+
+  methods: {
+    updateUser() {
+      const newUser = {
+      first_name: 'João',
+      last_name: 'Fernandes',
+      email: 'joao@gmail.com'
+    }
+    this.$store.commit('storeUser', newUser)
+  },
+
+
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,49 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    user: {
+      first_name: 'Bruna',
+      last_name: 'Carvalho',
+      email: 'bruna@gmail.com'
+    },
+    product: [
+      {
+        id: 1,
+        name: 'Pote',
+        price: 20
+      },
+      {
+        id: 2,
+        name: 'Xícara',
+        price: 40
+      },
+      {
+        id: 3,
+        name: 'cesto',
+        price: 35
+      }
+    ],
+    cart: [],
+  },
+  getters: {
+  },
+  mutations: {
+    storeUser(state, data) {
+    state.user = data
+    },
+    addProduct(state, data){
+      state.cart.push(data)
+    },
+
+    removeProduct(state, id) {
+      const index = state.cart.findIndex(objeto => objeto.id === id)
+      state.cart.splice(index, 1);
+
+    },
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
